7770079f5263d8526863039b84be07cc0f49c808
tanvirch0750 <tanvir0750@yahoo.com>
2020-09-02 12:03:05 +0600

    added a popup project

---
+++ popup/popup.js
@@ -0,0 +1,23 @@
+const parent = document.querySelector(".modal-parent");
+const btn = document.querySelector("button");
+const X = document.querySelector(".x");
+const section = document.querySelector("section");
+
+btn.addEventListener("click", appear);
+
+function appear() {
+  parent.style.display = "block";
+  section.style.filter = "blur(10px)";
+}
+X.addEventListener("click", disappearX);
+function disappearX() {
+  parent.style.display = "none";
+  section.style.filter = "blur(0px)";
+}
+parent.addEventListener("click", disappearParent);
+function disappearParent(e) {
+  if (e.target.className == "modal-parent") {
+    parent.style.display = "none";
+    section.style.filter = "blur(0px)";
+  }
+}
